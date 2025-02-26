f9888b1c8df235339a666d13a1f23569bf072130
sua <dssua0714@naver.com>
2025-02-26 11:35:47 +0900

    router_6.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom'
 import routes from './route/Routes'
 import Header from './components/Header'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -7,14 +7,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          {routes.map((route, index) => {
-            return <Route key={route.path || index} path={route.path} element={route.element}></Route>
-          })}
-        </Routes>
-      </BrowserRouter>
+      <div>헤더입니다.</div>
+      <Outlet />
+      <div>꼬리말</div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
-//import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-//import App from './App.jsx'
+import App from './App.jsx'
 import App2 from './App2.jsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './route/Router.jsx'
 
 createRoot(document.getElementById('root')).render(
-  //<StrictMode>
-    //<App />,
-    <App2 />,
-  //</StrictMode>,
+  <RouterProvider router={router} />
 )
